f9313236235406ac138d864eb805315aedb60283
Lais Ferrari <laisferrari.lf@gmail.com>
2026-06-26 21:59:38 -0300

    chore: configura projeto Spring Boot

---
+++ src/main/java/com/mindjournal/MindJournalApplication.java
@@ -0,0 +1,12 @@
+package com.mindjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindJournalApplication.class, args);
+    }
+}
